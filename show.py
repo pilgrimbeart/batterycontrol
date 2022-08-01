3be8fcc807c38ba44ba54a733635bd5589d3e5dd
Pilgrim Beart <pilgrim@beart.org.uk>
2022-08-01 14:50:14 +0100

    Clearer tabulation

--- show.py
+++ show.py
@@ -1,12 +1,49 @@
 import json, time
+import sys
 import utcstuff, filer
+
+READING_PERIOD_S = 5*60
+
+def render(v):
+    if type(v) == float:
+        return "%1.3f" % v
+    return str(v)
 
 if __name__ == "__main__":
     readings = filer.read_file("readings", utcstuff.todays_date_iso8601())
+    widths = {}
     for r in readings:
-        try:
-            print(time.ctime(r["end"]),
-                    max(0,r["import"]-r["house"]),
-                    r)
-        except:
-            print("error")
+        for (k,v) in r.items():
+            w = len(render(v))
+            if k not in widths:
+                widths[k] = w
+            else:
+                widths[k] = max(w, widths[k])
+
+    for w in widths:    # Ensure each column at least wide-enough for column name!
+        widths[w] = max(widths[w], len(str(w)))
+        
+    names = sorted(widths)
+
+    for name in names:
+        print(name, end="")
+        print(" " * (widths[name]-len(name)), end="")
+        print("|", end="")
+    print("UTC")
+
+    for r in readings:
+        if len(r) == 0:
+            continue    # Don't print blank lines
+        for n in names:
+            if n not in r:
+                print(" " * widths[n], end="")
+            else:
+                s = render(r[n])
+                print(s, end="")
+                print(" " * (widths[n]-len(s)), end="")
+            print("|", end="")
+
+        secs = r["reading_number"] * READING_PERIOD_S
+        hrs = int(secs / (60*60))
+        mins = int((secs % (60*60)) / 60)
+        print("%02d:%02d" % (hrs, mins) )
